dd9e8151cf2109db85e1fdbb214999091588eb00
takuya04 <takuya.ct0418@gmail.com>
2021-08-25 14:41:41 +0900

    sample at 8/25

---
+++ src/test/java/com/airs/demo/SampleOf825ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleOf825ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
